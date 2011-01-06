ef5ac0c17320aafe207bc70022cddc3d9e765d98
davidajnered <david@goodold.se>
2011-01-06 14:47:08 +0100

    Added js to thumbnail

--- js/simple-post-preview.js
+++ js/simple-post-preview.js
@@ -2,5 +2,23 @@
  * Implement javascript and ajax stuff
  */
 jQuery('document').ready(function() {
-  
+
+  initCheckbox();
+
+  jQuery('#widget-simple_post_preview-3-thumbnail').live('click', function() {
+    if(jQuery(this).is(':checked')) {
+      jQuery('#widget-simple_post_preview-3-thumbnail_size').parent('p').show();
+    } else {
+      jQuery('#widget-simple_post_preview-3-thumbnail_size').parent('p').hide();
+    }
+  });
+
 });
+
+function initCheckbox() {
+  if(jQuery(this).is(':checked')) {
+    jQuery('#widget-simple_post_preview-3-thumbnail_size').parent('p').show();
+  } else {
+    jQuery('#widget-simple_post_preview-3-thumbnail_size').parent('p').hide();
+  }
+}
